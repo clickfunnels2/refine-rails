d6d31baf7b31d97e8f92f39ca5c3decf2604c8a8
Sean Fioritto <sean@planningforaliens.com>
2021-03-17 08:33:29 -0500

    Remove unused action and private methods
    (transferred from clickfunnels2/admin@25f3cafc5b425091f69ebee4b0fe6d7174244073)

--- app/controllers/hammerstone/refine_blueprints_controller.rb
+++ app/controllers/hammerstone/refine_blueprints_controller.rb
@@ -5,22 +5,6 @@ class Hammerstone::RefineBlueprintsController < ApplicationController
     @refine_filter = filter
   end
 
-  def create
-    @refine_filter = filter
-
-    respond_to do |format|
-      format.turbo_stream do
-        if group
-          render turbo_stream: turbo_stream.append(:groups, partial: 'group', locals: group)
-        elsif criterion
-          render turbo_stream: turbo_stream.append(
-            "criteria_#{criterion[:group_id]}", partial: 'criterion', locals: criterion)
-        end
-      end
-      format.html
-    end
-  end
-
   private
 
   def filter
@@ -29,23 +13,7 @@ class Hammerstone::RefineBlueprintsController < ApplicationController
     filterClass.new(blueprint)
   end
 
-  def filter_name
-    filter_params[:filter]
-  end
-
-  def blueprint
-    filter_params[:blueprint]
-  end
-
   def filter_params
     params.permit(:filter, :blueprint)
-  end
-
-  def group
-    params.has_key?(:group) && JSON.parse(params.require(:group)).deep_symbolize_keys
-  end
-
-  def criterion
-    params.has_key?(:criterion) && JSON.parse(params.require(:criterion)).deep_symbolize_keys
   end
 end

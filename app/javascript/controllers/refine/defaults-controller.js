e2e496f6884c755197abfeee9ded537f05a0c6a2
Sean Fioritto <sean@planningforaliens.com>
2021-04-09 09:10:26 -0500

    Create a controller to allow clauses to set a default input value when they load in.
    (transferred from clickfunnels2/admin@e4e72df597897d3eff5aecbe60e04cfc40fe0ce1)

---
+++ app/javascript/controllers/refine/defaults-controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static values = {
+    criterionId: Number,
+    input: Object,
+  };
+
+  connect() {
+    const refineElement = document.getElementById('refine');
+
+    this.state = this.application.getControllerForElementAndIdentifier(
+      refineElement,
+      'refine--state',
+    );
+
+    this.state.updateInput(
+      this.criterionIdValue,
+      this.inputValue,
+    );
+  }
+}

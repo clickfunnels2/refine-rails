0c45b6a6f298a9e8304b18595e93606f08e956e5
David Biagi <david.biagi@clickfunnels.com>
2022-03-11 12:09:16 -0600

    WIP: workflows
    (transferred from clickfunnels2/admin@0e628bd0dd4997596b9232bba9f46d6a4e6d97d4)

--- app/models/hammerstone/refine/stabilizers/stored_filter.rb
+++ app/models/hammerstone/refine/stabilizers/stored_filter.rb
@@ -1,5 +1,9 @@
 module Hammerstone::Refine::Stabilizers
   class StoredFilter < WorkspaceRecord
     self.table_name = "hammerstone_refine_stored_filters"
+
+    shareable do
+      exclude_sharing_attributes %w[id created_at updated_at]
+    end
   end
 end

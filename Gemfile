72197324d1ae997a5052c2e5e1080f7c0b622e6d
leenyburger <colleen@bitmappeddesigns.com>
2022-04-18 09:26:16 -0700

    Fix tests - WIP

--- Gemfile
+++ Gemfile
@@ -8,5 +8,10 @@ gem "sqlite3"
 
 gem "sprockets-rails"
 
+group :test do 
+	gem "minitest-around"
+  gem "minitest-ci"
+end
+
 # Start debugger with binding.b [https://github.com/ruby/debug]
 # gem "debug", ">= 1.0.0"
--- test/support/hammerstone/cf2_stabilize_filter.rb
+++ test/support/hammerstone/cf2_stabilize_filter.rb
@@ -1,20 +1,20 @@
-class Cf2StabilizeFilter < ApplicationFilter
-  def automatically_stabilize?
-    true
-  end
+# class Cf2StabilizeFilter < ApplicationFilter
+#   def automatically_stabilize?
+#     true
+#   end
 
-  def t(key, options = {})
-    I18n.t("scaffolding/completely_concrete/tangible_things#{key}", options)
-  end
+#   def t(key, options = {})
+#     I18n.t("scaffolding/completely_concrete/tangible_things#{key}", options)
+#   end
 
-  def table
-    Scaffolding::CompletelyConcrete::TangibleThing.arel_table
-  end
+#   def table
+#     Scaffolding::CompletelyConcrete::TangibleThing.arel_table
+#   end
 
-  def conditions
-    [
-      Hammerstone::Refine::Conditions::TextCondition.new("button_value").only_clauses([Hammerstone::Refine::Conditions::TextCondition::CLAUSE_SET]),
-      Hammerstone::Refine::Conditions::TextCondition.new("text_field_value").only_clauses([Hammerstone::Refine::Conditions::TextCondition::CLAUSE_STARTS_WITH])
-    ]
-  end
-end
+#   def conditions
+#     [
+#       Hammerstone::Refine::Conditions::TextCondition.new("button_value").only_clauses([Hammerstone::Refine::Conditions::TextCondition::CLAUSE_SET]),
+#       Hammerstone::Refine::Conditions::TextCondition.new("text_field_value").only_clauses([Hammerstone::Refine::Conditions::TextCondition::CLAUSE_STARTS_WITH])
+#     ]
+#   end
+# end

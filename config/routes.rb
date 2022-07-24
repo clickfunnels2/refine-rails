7df72c9449e21fff452f729e88d8be83a4e248d6
leenyburger <colleen@bitmappeddesigns.com>
2022-07-24 13:04:45 -0700

    Add the routes to the gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,11 @@
 Rails.application.routes.draw do
+  namespace :hammerstone do
+    resource :refine_blueprint, only: [:show, :update, :create]
+    put "update_stable_id", to: "refine_blueprints#update_stable_id"
+    namespace :refine do
+      resources :stored_filters, only: [:create, :index, :show, :new, :update, :edit] do
+        get "editor", on: :collection
+      end
+    end
+  end
 end

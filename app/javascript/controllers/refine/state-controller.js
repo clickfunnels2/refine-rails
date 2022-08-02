ba6e33c5cb1a80a912ddc2efa5d219fcb81de176
leenyburger <colleen@bitmappeddesigns.com>
2022-08-02 10:08:13 -0700

    Upgrade to Stimulus 3

--- app/javascript/controllers/refine/state-controller.js
+++ app/javascript/controllers/refine/state-controller.js
@@ -1,4 +1,4 @@
-import { Controller } from "stimulus"
+import { Controller } from "@hotwired/stimulus"
 import { delegate, abnegate } from 'jquery-events-to-dom-events'
 import { filterUnstableEvent, filterStabilizedEvent, blueprintUpdatedEvent } from '../../refine/helpers'
 

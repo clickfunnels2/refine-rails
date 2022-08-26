3deeefe7d2fd6f828111992ffedb3b55c7a786a8
Colleen <leenyburger@users.noreply.github.com>
2022-08-26 16:24:17 -0700

    Add helper methods (#43)
    * Add filter button back in
    
    * Add search filter controller
    
    * Update changelog

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,6 +7,7 @@ import FormController from './refine/form-controller'
 import StateController from './refine/state-controller'
 import StoredFilterController from './refine/stored-filter-controller'
 import UpdateController from './refine/update-controller'
+import SearchFilterController from './search-filter-controller'
 
 export const controllerDefinitions = [
   [AddController, 'refine/add-controller.js'],
@@ -15,7 +16,8 @@ export const controllerDefinitions = [
   [FormController, 'refine/form-controller.js'],
   [StateController, 'refine/state-controller.js'],
   [StoredFilterController, 'refine/stored-filter-controller.js'],
-  [UpdateController, 'refine/update-controller.js']
+  [UpdateController, 'refine/update-controller.js'],
+  [SearchFilterController, 'search-filter-controller.js']
 ].map(function(d) {
   const key = d[1]
   const controller = d[0]
@@ -32,5 +34,6 @@ export {
   FormController,
   StateController,
   StoredFilterController,
-  UpdateController
+  UpdateController, 
+  SearchFilterController
 }

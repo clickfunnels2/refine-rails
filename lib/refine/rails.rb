3ae28bb0493a3ec8f672ebe8172f35b9a6011132
Colleen <leenyburger@users.noreply.github.com>
2022-11-11 11:13:56 -0800

    Allow for stored filter scoping (#75)
    * Allow for setting custom attributes on stored filters in initializer
    
    * Updated stored filter to accept custom attributes

--- lib/refine/rails.rb
+++ lib/refine/rails.rb
@@ -3,10 +3,11 @@ require "refine/rails/engine"
 
 module Refine
   module Rails
-    class Config < Struct.new(:stored_filter_scope, keyword_init: true); end
+    class Config < Struct.new(:stored_filter_scope, :custom_stored_filter_attributes, keyword_init: true); end
 
     @configuration = Config.new(
-      stored_filter_scope: ->(scope) { scope }
+      stored_filter_scope: ->(scope) { scope },
+      custom_stored_filter_attributes: -> {{}}
     )
     module_function def configuration
       @configuration

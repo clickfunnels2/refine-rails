c91610c21ed60bce762f4e5093de4df2be2a0fe4
Rafe Rosen <rafe@existentialmutt.com>
2022-11-28 12:20:57 -0500

    Fix filter pills reload

--- app/javascript/controllers/refine/filter-pills-controller.js
+++ app/javascript/controllers/refine/filter-pills-controller.js
@@ -1,6 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
+import { FetchRequest } from '@rails/request.js'
 
 export default class extends Controller {
+  static values = {
+    submitUrl: String
+  }
 
   connect() {
     const urlParams = new URLSearchParams(window.location.search)
@@ -17,10 +21,19 @@ export default class extends Controller {
 
   async reloadPage() {
     const {blueprint} = this.stateController
-    const validationResult = await this.stabilizeFilterController.validateBlueprint(blueprint)
-    if (validationResult.stableId) {
-      this.redirectToStableId(validationResult.stableId)
-    }
+    const request = new FetchRequest(
+      "POST",
+      this.submitUrlValue,
+      {
+        responseKind: "turbo-stream",
+        body: JSON.stringify({
+          filter: this.stateController.filterName,
+          blueprint: JSON.stringify(blueprint),
+          form_id: this.stateController.formIdValue
+        })
+      }
+    )
+    await request.perform()
   }
 
   redirectToStableId(stableId) {

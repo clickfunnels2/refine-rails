ce7bc9eda96b982c4537e68563b5d70b8403e75a
Rafe Rosen <rafe@existentialmutt.com>
2023-02-14 19:19:42 -0500

    Update configuration of stabilizer classes (#89)
    * Default to gem-provided stabilizers
    
    * WIP update stabilizer class confgi
    
    * Load stabilizer classes with library

--- lib/refine/rails.rb
+++ lib/refine/rails.rb
@@ -1,16 +1,27 @@
 require "refine/rails/version"
 require "refine/rails/engine"
+require_relative "../../app/models/hammerstone/refine/stabilizers/database_stabilizer"
+require_relative "../../app/models/hammerstone/refine/stabilizers/url_encoded_stabilizer"
 
 module Refine
   module Rails
-    class Config < Struct.new(:stored_filter_scope, :custom_stored_filter_attributes, keyword_init: true); end
+    class Config < Struct.new(:stored_filter_scope, :custom_stored_filter_attributes, :stabilizer_classes, keyword_init: true); end
 
     @configuration = Config.new(
       stored_filter_scope: ->(scope) { scope },
-      custom_stored_filter_attributes: -> {{}}
+      custom_stored_filter_attributes: -> {{}},
+      stabilizer_classes: {
+        db: Hammerstone::Refine::Stabilizers::DatabaseStabilizer,
+        url: Hammerstone::Refine::Stabilizers::UrlEncodedStabilizer
+      }
     )
+
     module_function def configuration
-      @configuration
+      if block_given?
+        yield @configuration
+      else
+        @configuration
+      end
     end
   end
 end

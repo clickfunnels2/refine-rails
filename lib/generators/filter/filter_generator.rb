d8c3aee028779038572b80018a6498b329467392
Ryan Castillo <rmcastil@gmail.com>
2023-02-28 10:00:05 -0500

    Use generator for creating filters
    Now works with the following pattern
    
    rails generate filter model_name field_name:numeric

---
+++ lib/generators/filter/filter_generator.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# Generates a filter for a given model.
+class FilterGenerator < Rails::Generators::NamedBase
+  CONDITIONS = %w[text date numeric].freeze
+  source_root File.expand_path('templates', __dir__)
+
+  def create_filter
+    @conditions = extract_conditions(args)
+    template 'filter.rb.erb', "app/filters/#{file_name}_filter.rb"
+  end
+
+  private
+
+  def extract_conditions(args)
+    conditions = []
+    args.each do |arg|
+      field, condition = arg.split(':')
+
+      throw "#{condition} is an invalid condition" unless CONDITIONS.include? condition
+
+      conditions << { field: field, condition: condition.capitalize }
+    end
+
+    conditions
+  end
+end

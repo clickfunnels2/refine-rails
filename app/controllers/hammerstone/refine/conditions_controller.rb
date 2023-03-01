6ea50760c3ee78e3897737e80c5d1bed8872cd42
Rafe Rosen <rafe@existentialmutt.com>
2023-03-01 14:47:00 -0500

    Update criteria

--- app/controllers/hammerstone/refine/conditions_controller.rb
+++ app/controllers/hammerstone/refine/conditions_controller.rb
@@ -49,17 +49,31 @@ class Hammerstone::Refine::ConditionsController < ApplicationController
       }
     }
 
-    # set url
+    redirect_to_stable_id(refine_filter.to_stable_id)
+  end
+
+  def update
+    refine_filter = @refine_filter_builder.refine_filter
+    blueprint = refine_filter.blueprint
+
+    criterion_data = blueprint[params[:id].to_i]
+    criterion_data[:input][:clause] = params[:clause]
+    criterion_data[:input][:value] = params[params[:condition_id]]
+    redirect_to_stable_id(refine_filter.to_stable_id)
+  end
+
+  private
+
+  def redirect_to_stable_id stable_id
+    # update_stable_id in url
     uri = URI(request.referrer)
     new_query_ar = URI.decode_www_form(String(uri.query))
     new_query_ar.reject! { |(k, _v)| k == "stable_id" }
-    new_query_ar << ["stable_id", refine_filter.to_stable_id]
+    new_query_ar << ["stable_id", stable_id]
     uri.query = URI.encode_www_form(new_query_ar)
     
     redirect_to uri.to_s
   end
-
-  private
 
   def set_builder
     builder_params = params.require(:hammerstone_refine_filters_builder).permit(

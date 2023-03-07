d05715742b5bbe8121e535703e7aa310d4c809d2
leenyburger <colleen@bitmappeddesigns.com>
2023-03-07 11:40:12 -0800

    Move automatically stabilize to filter class because urlencoded stabilization is required

--- app/models/hammerstone/refine/filter.rb
+++ app/models/hammerstone/refine/filter.rb
@@ -45,10 +45,15 @@ module Hammerstone::Refine
       @initial_query
     end
 
+    def automatically_stabilize?
+      true
+    end
+
     # If the initial condition has not been set in the filter, validation for relationships will error.
     # The BT implementation builds the filter *then* sets the initial condition
     # This pulls a "smart" default from the filter arel table - Arel::Table provides the table name, can extract the model then call "all"
     # to return and AR::R object which is what `uses_attributes` expects.
+    # TODO: This breaks for namespaced classes.
     def fallback_initial_condition
       # TODO: Figure out a better way to do this.
       table.name.classify.constantize.all

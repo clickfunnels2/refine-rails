740ac1c6625ae351ace0a2806c081ce1fcc70197
Rafe Rosen <rafe@existentialmutt.com>
2023-09-18 19:41:22 -0400

    add turbo-stream-link controller (#138)

---
+++ app/javascript/controllers/refine/turbo-stream-link-controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+import { FetchRequest } from '@rails/request.js'
+
+/*
+  attach to a link element to have it request turbo stream responses
+
+  <a href="/contacts" data-controller="refine--turbo-stream-link" data-action="refine--turbo-stream-link#get">Click me</a>
+
+  Turbo is supposed to handle this natively with data-turbo-stream but we're
+  seeing issues using that attribute inside iframes
+*/
+export default class extends Controller {
+  async get(event) {
+    event.preventDefault()
+    const request = new FetchRequest(
+      "GET",
+      this.element.href,
+      {
+        responseKind: "turbo-stream",
+      }
+    )
+    await request.perform()
+  }
+}

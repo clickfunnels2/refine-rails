7d8a78d3a5516d29d01aea83b1e1bb8695ea165d
Rafe Rosen <rafe@existentialmutt.com>
2023-10-31 11:57:25 -0400

    Allow dismissal of inline QA popups (#145)
    * dismiss popup with clickoutside
    
    * dismiss inline popups with escape key

--- app/javascript/controllers/refine/popup-controller.js
+++ app/javascript/controllers/refine/popup-controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
+import { useClickOutside } from 'stimulus-use'
 
 // simple controller to hide/show the filter modal
 export default class extends Controller {
@@ -8,13 +9,33 @@ export default class extends Controller {
     src: String
   }
 
+  connect() {
+    useClickOutside(this)
+    this.boundHandleKeyUp = this.handleKeyUp.bind(this)
+    document.addEventListener("keyup", this.boundHandleKeyUp)
+  }
+
+  disconnect() {
+    document.removeEventListener("keyup", this.boundHandleKeyUp)
+  }
+
   show(event) {
     event.preventDefault()
     this.frameTarget.src = this.srcValue;
   }
 
   hide(event) {
-    event.preventDefault()
+    event?.preventDefault()
     this.frameTarget.innerHTML = "";
+  }
+
+  clickOutside(event) {
+    this.hide(event)
+  }
+
+  handleKeyUp(event) {
+    if (event.key === "Escape" || event.key === "Esc") {
+      this.hide(event)
+    }
   }
 }

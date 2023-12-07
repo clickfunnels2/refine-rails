43a56042192643edc8a8d114e588fde741d2e02f
Rafe Rosen <rafe@existentialmutt.com>
2023-12-07 11:22:40 -0500

    Add typeahead to option conditions, get form refresh for clauses working

--- app/javascript/controllers/refine/typeahead-list-controller.js
+++ app/javascript/controllers/refine/typeahead-list-controller.js
@@ -11,7 +11,7 @@ export default class extends Controller {
     // hide / show listItem links that match the query and note which
     // categories should be visible
     this.listItemTargets.forEach(listItemNode => {
-      const listItemName = listItemNode.innerHTML.toLowerCase()
+      const listItemName = listItemNode.data.listItemValue.toLowerCase()
       if (listItemName.includes(query)) {
         listItemNode.hidden = false
         visibleCategories.add(listItemNode.dataset.category)

ee6fe6ce75bbaac1a0d2d58238cd59dd8c4bfedb
Kasper Timm Hansen <kaspth@gmail.com>
2023-12-10 21:36:16 +0100

    Extract internal I18n handling into a Refine convention
    This lets apps define headings and options in I18n YML files.
    
    So if you have a `ContactsFilter`, `ContactFilter`, or `Contact::Filter` filter,
    you can define these keys:
    
    ```
    en:
      contacts:
        fields:
          status:
            heading: Status
            options:
              drafted: Drafted
              published: Published
    ```
    
    And then have those values fetched via something like this:
    
    ```ruby
    class Contact::Filter
      def conditions
        [OptionCondition.new(:status).with_display(heading(:status)).with_options(options_for(:status))]
      end
    end
    ```

---
+++ app/models/refine/filter/internationalized.rb
@@ -0,0 +1,28 @@
+module Refine::Filter::Internationalized
+  extend ActiveSupport::Concern
+
+  included do
+    class_attribute :i18n_scope, instance_writer: false
+  end
+
+  class_methods do
+    def inherited(klass)
+      klass.i18n_scope = klass.model_name.pluralize.underscore.tr("/", ".")
+      super
+    end
+  end
+
+  private
+
+  def heading(field)
+    t("#{field}.heading")
+  end
+
+  def options_for(field)
+    t("#{field}.options").map { {id: _1.to_s, display: _2} }
+  end
+
+  def t(key, **)
+    I18n.t("#{i18n_scope}.fields.#{key}", **)
+  end
+end

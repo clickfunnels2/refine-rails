2f86793b40cfe081e35136472af829f9dacf80ac
Steven Steffen <spsteffen1s@gmail.com>
2024-03-04 13:38:30 -0600

    fixing issue with the ago and from now modifier not being passed into inline filters

--- app/models/refine/inline/criteria/date_refinement.rb
+++ app/models/refine/inline/criteria/date_refinement.rb
@@ -1,14 +1,15 @@
 class Refine::Inline::Criteria::DateRefinement
   include ActiveModel::Model
 
-  attr_accessor :clause, :date1, :date2, :days
+  attr_accessor :clause, :date1, :date2, :days, :modifier
 
   def attributes
     {
       clause: clause,
       date1: date1,
       date2: date2,
-      days: days
+      days: days,
+      modifier: modifier
     }.compact
   end
 end

9b9f86f39a9971d02daf39fcb6be7f94c79f96db
Steven Steffen <spsteffen1s@gmail.com>
2024-11-18 11:54:46 -0600

    wip

--- app/javascript/controllers/refine/inline-advanced-modal-controller.js
+++ app/javascript/controllers/refine/inline-advanced-modal-controller.js
@@ -2,14 +2,65 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-  static targets = ['searchBarInput']
+  static targets = ['searchBarInput', 'categoryListItem', 'categoryBlockItem', 'categoryShortcutItem']
 
-  showSearchBar(event) {
+  connect() {
+    this.observer = new IntersectionObserver(
+      this.handleIntersection.bind(this), {
+        threshold: 1
+      }
+    )
+
+    this.categoryBlockItemTargets.forEach(item => this.observer.observe(item))
+  }
+
+  disconnect() {
+    this.observer.disconnect()
+  }
+
+  handleIntersection(entries) {
+    console.log("handleIntersection", entries)
+    entries.forEach(entry => {
+      if(entry.isIntersecting) {
+        console.log("intersecting", entry.target.dataset.categoryListBlockValue)
+        this.highlightCategory(entry.target.dataset.categoryListBlockValue)
+      }
+    })
+  }
+
+  highlightCategory(categoryName) {
+    this.categoryShortcutItemTargets.forEach(item => {
+      if(item.dataset.inlineAdvancedModalValue === categoryName) {
+        item.classList.add('active')
+      } else {
+        item.classList.remove('active')
+      }
+    })
+  }
+
+  showSearchBar() {
     this.searchBarInputTarget.hidden = false
   }
 
-  hideSearchBar(event) {
+  hideSearchBar() {
     this.searchBarInputTarget.hidden = true
+  }
+
+  findCategoryElementByName(categoryName) {
+    // Use the find method to locate the target with the specified attribute value
+    return this.categoryListItemTargets.find(item => item.dataset.categoryListItemValue === categoryName)
+  }
+
+  scrollToCategory(event) {
+    const categoryName = event.target.dataset.inlineAdvancedModalValue
+    const categoryElement = this.findCategoryElementByName(categoryName)
+    if(categoryElement) {
+      categoryElement.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest"
+      })
+    }
   }
   
 }
